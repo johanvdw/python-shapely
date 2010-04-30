e2c019839e9a8efe62f99c64f6bee9cba42fc7ad
Pietro Battiston <toobaz@email.it>
2010-04-30 12:21:53 +0200

    Imported Upstream version 1.0.15

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if version_info[:2] < (2,5):
 readme_text = file('README.txt', 'rb').read()
 
 setup(name          = 'Shapely',
-      version       = '1.0.14',
+      version       = '1.0.15',
       description   = 'Geospatial geometries, predicates, and operations',
       license       = 'BSD',
       keywords      = 'geometry topology',
